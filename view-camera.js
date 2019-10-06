1b0c7d9340f22812d02b165c30d0920c9bb94875
Alex W <alexfromapex@gmail.com>
2019-10-06 17:17:13 -0400

    Initial commit

---
+++ view-camera.js
@@ -0,0 +1,26 @@
+var constraints = {audio: false, video: true};
+var video = document.querySelector("video");
+
+function successCallback(stream) {
+  video.srcObject = stream;
+  video.play();
+}
+
+function errorCallback(error) {
+  console.log("navigator.getUserMedia error: ", error);
+}
+
+navigator.mediaDevices.getUserMedia(constraints)
+  .then(successCallback)
+  .catch(errorCallback);
+  
+  var elem = document.querySelector("video");
+if (elem.requestFullscreen) {
+  elem.requestFullscreen();
+} else if (elem.msRequestFullscreen) {
+  elem.msRequestFullscreen();
+} else if (elem.mozRequestFullScreen) {
+  elem.mozRequestFullScreen();
+} else if (elem.webkitRequestFullscreen) {
+  elem.webkitRequestFullscreen();
+}
